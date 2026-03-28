e3e166901fc856a2692832e5f998c594400470b4
Karthi-mr <mkarthim1998@gmail.com>
2026-03-28 08:44:08 +0530

    removed unwanted imports

--- 05-graphql-demo/backend/src/test/java/com/learn/graphql/book/BookControllerTest.java
+++ 05-graphql-demo/backend/src/test/java/com/learn/graphql/book/BookControllerTest.java
@@ -2,13 +2,9 @@ package com.learn.graphql.book;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.graphql.test.autoconfigure.AutoConfigureGraphQl;
-import org.springframework.boot.graphql.test.autoconfigure.GraphQlTest;
 import org.springframework.boot.graphql.test.autoconfigure.tester.AutoConfigureGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.graphql.test.tester.GraphQlTester;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @AutoConfigureGraphQlTester
